54a83fd078fd38e06bb6004c2dbfd5baacec5bd8
George Davidson <gd240673@gmail.com>
2023-02-13 21:48:43 -0500

    Update

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import git
 import requests
 
-github_user = "MrDingleDucky"
+github_user = "DrDingleDucky"
 
 response = requests.get(f"https://api.github.com/users/{github_user}/repos")
 repo_urls = [repo["html_url"] for repo in response.json()]
 
-index = 1
-
-for repo_url in repo_urls:
+for i, repo_url in enumerate(repo_urls):
     repo_name = repo_url.split("/")[-1].replace(".get", "")
     git.Repo.clone_from(repo_url, repo_name)
-    print(f"{index}: Cloned '{repo_name}' SUCCESSFULLY")
-    index += 1
+    print(f"{i + 1}: Cloned '{repo_name}' SUCCESSFULLY")
 
 print("Done")
